fdd5b8f6c465a2a236bba39293026848359c668d
unknown <khadijehshah78@gmail.com>
2022-02-21 22:16:20 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# print("enter number")
+
+
+
+# w=int(input("number"))
+# list=[]
+# for i in range(w):
+   
+#   p=int(input('num:'))
+#   list.append(p)
+# # list2=sorted(list)
+#   list.sort()
+#   print(list)
+    
+    
+# print("---------------------------")
+
+# base = float(input("length of base: "))
+# height = float(input('measurement of height: '))
+# area = base * height
+# print("masahat", area)
+
+# print("---------------------------")
+
+    
+
+# x=5
+# for i in range (1,x):
+#   for j in range (1,i+1):
+#       print("*", end=' ')
+#   print()
+
+# print("----------------------------")
+# y=6
+# for i in range (1,y):
+#   for j in range (i,y):
+#       print("*", end=' ')
+#   print()
+# print("----------------------------")
+# z=6
+# for i in range (1,z):
+#   for j in range (i):
+#       print(" ", end=' ')
+#   for j in range(i,z):
+#       print("*", end=' ')
+
+#   print()
+
+# x=int(input("enter a number"))
+# y=int(input("enter a number"))
+# z=int(input("enter a number"))
+# min=0
+
+# if min<x:
+#    if x<y and x<z:
+#      min=x
+#      print(min)
+#      min=0
+   
+# elif min<y:
+#    if y<x and y<z:
+#       min=y
+#       print(min)
+#       min=0
+    
+  
+# elif min<z:    
+#    if z<x and z<z:
+#       min=z
+#       print(min)
+#       min=0
+
